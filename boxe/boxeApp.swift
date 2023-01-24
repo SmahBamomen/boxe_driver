0f3f275f8e0acfe5ca4570dea3abfcf1595a6e1e
smahbamomen <smahbamomen@gmail.com>
2023-01-24 13:43:37 +0300

    Initial Commit

---
+++ boxe/boxeApp.swift
@@ -0,0 +1,17 @@
+//
+//  boxeApp.swift
+//  boxe
+//
+//  Created by Samah Bamumin on 02/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct boxeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
